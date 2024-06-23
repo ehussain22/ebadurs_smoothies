c713726cbed54e326a1997d2d6093d7e13fbbfff
ehussain22 <ebadurosib@gmail.com>
2024-06-23 16:19:21 +0100

    Update orderprocess.py

--- orderprocess.py
+++ orderprocess.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 st.title("Pending Smoothie Orders :cup_with_straw:")
